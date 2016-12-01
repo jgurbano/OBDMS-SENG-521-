eba62b2089ccc552442a7a3fdd00ac1341e09d25
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2016-12-01 13:37:39 -0700

    Added frontend folder with test login.html file

--- Backend/mainServer.js
+++ Backend/mainServer.js
@@ -7,6 +7,14 @@ var webpagePath = path.join(__dirname, "../Frontend");
 
 webServer.use(express.static(webpagePath));
 
+webServer.get('/', function(req, res){
+	res.redirect('/login');
+});
+
+webServer.get('/login', function(req, res){
+	res.sendFile(webpagePath + "/login.html");
+})
+
 webServer.listen(8080, function(){
 	systemStatus('server is listening at 8080');
 });
